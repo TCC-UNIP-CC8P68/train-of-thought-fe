974f0429e489e919041febeb2f1a9c432994af56
Rodrigo Santos <rodrigo.santos@qflash.com.br>
2021-08-26 23:00:47 -0300

    Add user foreignkey

--- train-of-thought/public/background.js
+++ train-of-thought/public/background.js
@@ -20,13 +20,13 @@ function makeAnalysis(){
       if(tab) {
         console.log("URL Capturada: " + tab.url);
         console.log('Timeout: ' + timeoutValue);
-        postCapturedUrl(tab.url, momentOfCapture);
+        postCapturedUrl(1, tab.url, momentOfCapture);
       }
     });
   },timeoutValue);
 }
 
-async function postCapturedUrl(capturedUrl, momentOfCapture) {
+async function postCapturedUrl(userId, capturedUrl, momentOfCapture) {
   fetch('http://localhost:8084/capture', {
     method: 'POST',
     headers: {
@@ -34,20 +34,22 @@ async function postCapturedUrl(capturedUrl, momentOfCapture) {
       'Content-Type': 'application/json'
     },
     body: JSON.stringify({ 
+      "userId": userId,
       "capturedUrl": capturedUrl,
       "momentOfCapture": momentOfCapture
     })
   });
 }
 
-async function postConfiguration(setBy, timeoutValue) {
+async function postConfiguration(userId, setBy, timeoutValue) {
   fetch('http://localhost:8084/configuration', {
     method: 'POST',
     headers: {
       'Accept': 'application/json',
       'Content-Type': 'application/json'
     },
-    body: JSON.stringify({ 
+    body: JSON.stringify({
+      "userId": userId, 
       "setBy": setBy,
       "timeoutValue": timeoutValue
     })
@@ -64,7 +66,7 @@ chrome.runtime.onMessage.addListener(
   function(request) {
     setBy = request[0];
     timeoutValue = request[1]*1000;
-    postConfiguration(setBy, timeoutValue);
+    postConfiguration(1, setBy, timeoutValue);
 
     console.log('Valor do timeout definido para: ' + timeoutValue + " ms definido via: " + request[0]);
   }
