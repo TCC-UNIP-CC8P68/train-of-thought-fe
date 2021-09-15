941053c7394a8b864b15c48670d4181a048b52b5
RodySantoss <roodyy.roodyy@gmail.com>
2021-09-15 16:00:11 -0300

    Verifying if current url is a url Exception

--- src/public/background.js
+++ src/public/background.js
@@ -53,20 +53,28 @@ chrome.runtime.onMessage.addListener(
   }
 );
 
-function makeAnalysis(){
+async function makeAnalysis(){
   clearTimeout(analysisTimeout);
 
   analysisTimeout = setTimeout(async function(){
     if(allowCapture){
-      getCurrentTab().then(tab => {
-        let date = new Date();
-        let momentOfCapture = date.getTime();
-        if(tab) {
-          console.log("URL Capturada: " + tab.url);
-          console.log('Timeout: ' + timeoutValue);
-          postCapturedUrl(email, tab.url, momentOfCapture);
+      let captureUrl = true;
+      let tab = await getCurrentTab();
+      let urlExceptions = await getUrlException(email);
+      urlExceptions.forEach(element => {
+        console.log(element.url)
+        if(tab.url.match(element.url)) {
+          console.log("Url não pode ser capturada")
+          captureUrl = false;
         }
       });
+      if(captureUrl) {
+        let date = new Date();
+        let momentOfCapture = date.getTime();
+        console.log("URL Capturada: " + tab.url);
+        console.log('Timeout: ' + timeoutValue);
+        postCapturedUrl(email, tab.url, momentOfCapture);
+      }
     }
   },timeoutValue);
 }
