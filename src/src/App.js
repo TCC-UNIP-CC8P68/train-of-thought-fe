8e6a55fcf9632c107106549728e47245a099a802
Ruan Pimentel <ruanroxsasx@gmail.com>
2021-10-22 19:37:27 -0300

    feat: screen switch function

--- src/src/App.js
+++ src/src/App.js
@@ -10,8 +10,7 @@ function App() {
   return (
     <>
       <GlobalStorege>
-        <Extension />
-         {/* <Options /> */}
+         { window.innerHeight === 25 ? <Extension /> : <Options /> }
       </GlobalStorege>
     </>
   );
