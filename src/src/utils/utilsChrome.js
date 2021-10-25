aaf77dc63eae5944dfb2238677db6f77e4184e40
MatheusTurra <matheusfr.318@gmail.com>
2021-10-24 23:16:48 -0300

    Added user data to headerOptions / removed comments

--- src/src/utils/utilsChrome.js
+++ src/src/utils/utilsChrome.js
@@ -25,29 +25,3 @@ export async function getUserProfileData() {
   .then(responseData => responseData)
   .catch(error => console.log(error));
 }
-
-  /*
-  let userToken;
-  chrome.identity.getAuthToken({
-    interactive: true
-  }, token => {
-    if (chrome.runtime.lastError) {
-      console.log(chrome.runtime.lastError.message);
-      return false;
-    }
-
-    userToken = token;
-  });
-  console.log(userToken)
-  */
-  /*
-  let userData;
-  const url = "https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=" + token;
-  fetch(url)
-  .then(response => response.json())
-  .then(responseData => {
-    console.log(responseData)
-    return userData = responseData
-  }).catch(error => console.log(error));
-  console.log('MERDA DE FETCH', userData);
-  */
