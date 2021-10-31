97ce646f50259f7d2b65180840cdcdd30d5e5cc0
MatheusTurra <matheusfr.318@gmail.com>
2021-10-31 20:41:08 -0300

    Create captured links pagination

--- src/src/patterns/LinksPg/index.jsx
+++ src/src/patterns/LinksPg/index.jsx
@@ -1,18 +1,28 @@
 import React, { useEffect, useState } from 'react';
 import './LinksPg.scss';
+import ButtonCustom from '../../components/ButtonCustom';
 
 import { getUserUrlCaptures } from '../../utils/utilsApi';
 
 const LinksPg = () => {
   const [captures, setCaptures] = useState([]);
+  const [capturesApiOffset, setCapturesApiOffset] = useState(10);
 
   useEffect(async () => {
     const userCaptures = await getUserUrlCaptures(10, 0);
     setCaptures(userCaptures);
   }, []);
 
-  console.log(captures);
-  
+  const loadCaptures = async () => {
+    setCapturesApiOffset(capturesApiOffset + 10);
+    const newCaptures = await getUserUrlCaptures(10, capturesApiOffset);
+
+    if(newCaptures.length > 0) {
+      const updatedCaptures = captures.concat(newCaptures);
+      setCaptures(updatedCaptures);
+    } 
+  }
+
   return (
     <div className="block_options">
       <div className="div-block">
@@ -37,6 +47,7 @@ const LinksPg = () => {
             })}             
           </ul>
         </div>
+        <button className="pagination-button" onClick={loadCaptures}>Carregar mais posts</button>
       </div>
     </div>
   );
