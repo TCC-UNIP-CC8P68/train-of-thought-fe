c980c46972d20fedc0a33a26cccb00d7e97941f1
MatheusTurra <matheusfr.318@gmail.com>
2021-11-01 20:00:40 -0300

    Insert word cloud on dashboard

--- src/src/patterns/Dashboard/index.jsx
+++ src/src/patterns/Dashboard/index.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import wordCloud from '../../assets/wordCloud.png';
+
+import './Dashboard.scss';
 
 const Dashboard = () => {
   return (
     <div className="block_options">
       <div className="div-block">
         <h2> Dashboard</h2>
+        <img className="word-cloud" src={wordCloud} alt="word cloud"/>
       </div>
     </div>
   );
